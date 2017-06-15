4de1c56b1f4fff3d825dcef778cfa5387b898bc0
Kipp Morris <kipp.morris@gmail.com>
2017-06-14 22:37:14 -0400

    Added documentation to model classes

--- WhereIsMyStuff/app/src/main/java/cs2340/whereismystuff/model/User.java
+++ WhereIsMyStuff/app/src/main/java/cs2340/whereismystuff/model/User.java
@@ -1,12 +1,22 @@
 package cs2340.whereismystuff.model;
 
+/**
+ * Represents a user and contains all of the information about that user
+ */
 class User {
+
+    /**
+     * The user's information
+     */
     private String _firstName;
     private String _lastName;
     private String _email;
     private String _username;
     private String _password;
 
+    /**
+     * Creates a user with the information passed in
+     */
     User(String firstName, String lastName, String email, String username,
          String password) {
         _firstName = firstName;
@@ -16,23 +26,45 @@ class User {
         _password = password;
     }
 
+    /**
+     * Creates the administrator using a default set of information
+     */
     User() {
         this("admin", "", "admin@gatech.edu", "user", "pass");
     }
 
+    /**
+     * Returns the user's first and last name if they have a last name or just
+     * their first name if they don't
+     * @return The user's name
+     */
     String getName() {
         return (_lastName.length() == 0) ? _firstName :  _firstName + " " +
                 _lastName;
     }
 
+    /**
+     * Returns the user's email address
+     * @return The user's email address
+     */
     String getEmail() {
         return _email;
     }
 
+    /**
+     * Returns the user's username
+     * @return The user's username
+     */
     String getUsername() {
         return _username;
     }
 
+    /**
+     * Checks to see if the user has entered their password correctly
+     * @param  String password      The password the user has entered
+     * @return A boolean representing whether or not the user entered their
+     *         password correctly
+     */
     boolean checkPassword(String password) {
         return password.equals(_password);
     }
--- WhereIsMyStuff/app/src/main/java/cs2340/whereismystuff/model/UserManager.java
+++ WhereIsMyStuff/app/src/main/java/cs2340/whereismystuff/model/UserManager.java
@@ -2,26 +2,67 @@ package cs2340.whereismystuff.model;
 
 import java.util.HashMap;
 
+/**
+ * Represents a user manager that stores all of the users and is capable of
+ * validating user input and logging users in
+ */
 class UserManager {
+
+    /**
+     * A HashMap that maps usernames to the the corresponding users
+     */
     private HashMap<String, User> _users;
+
+    /**
+     * A HashMap that maps a email addresses to the corresponding users'
+     * usernames
+     */
     private HashMap<String, String> _emailUser;
+
+    /**
+     * The singular instance of UserManager that will be used by the Model to
+     * manage the users
+     */
     private static final UserManager instance = new UserManager();
 
+    /**
+     * Creates a UserManager containing only the default administrator
+     */
     private UserManager() {
         _users = new HashMap<>();
         _emailUser = new HashMap<>();
         setUp();
     }
 
+    /**
+     * Returns the user manager
+     * @return The user manager
+     */
     static UserManager getInstance() {
         return instance;
     }
 
+    /**
+     * Adds the administrator to the new, empty UserManager
+     */
     private void setUp() {
         User user = new User();
         addUser(user);
     }
 
+    /**
+     * Determines whether or not the information the user has entered is valid
+     * and returns 0 if it is valid and some other number if it is not
+     * @param  String firstName     The user's first name
+     * @param  String lastName      The user's last name
+     * @param  String email         The user's email
+     * @param  String username      The user's username
+     * @param  String password1     The user's password
+     * @param  String password2     Verification of the user's password                                                            password2 [description]
+     * @return A number that the controller will eventually use in
+     *         RegisterActivity to display a message to the user that will
+     *         explain how to fix their input if it is invalid
+     */
     private int validateInput(String firstName, String lastName, String email,
                               String username, String password1, String
                                       password2) {
@@ -51,11 +92,26 @@ class UserManager {
         }
     }
 
+    /**
+     * Adds the information of the user passed in to _users and _emailUser
+     * @param User user The user whose information is to be stored
+     */
     private void addUser(User user) {
         _users.put(user.getUsername(), user);
         _emailUser.put(user.getEmail(), user.getUsername());
     }
 
+    /**
+     * Takes in all of the information required to create a user, makes a new
+     * user, and stores that user's information in _users and _emailUser
+     * @param  String firstName     The user's first name
+     * @param  String lastName      The user's last name
+     * @param  String email         The user's email
+     * @param  String username      The user's username
+     * @param  String password1     The user's password
+     * @param  String password2     Verification of the user's password
+     * @return The integer code from validateInput
+     */
     int addUser(String firstName, String lastName, String email, String
             username, String password1, String password2) {
         firstName = firstName.trim();
@@ -70,6 +126,15 @@ class UserManager {
         return code;
     }
 
+    /**
+     * Attempts to login the user and returns an integer that will be used by
+     * the controller to know what message to display to the user based on the
+     * results of their login attempt
+     * @param  String usernameEmail The username/email the user has entered
+     * @param  String password      The password the user has entered
+     * @return A number that will eventually be used by the controller in
+     *         LoginActivity to determine what message to display to the user
+     */
     int loginUser(String usernameEmail, String password) {
         usernameEmail = usernameEmail.trim();
         User user;
@@ -94,6 +159,12 @@ class UserManager {
         }
     }
 
+    /**
+     * Takes in the user's username or email and uses it to retrieve their name
+     * from _users
+     * @param  String usernameEmail The username/email to be used
+     * @return The user's name if the user exists or "Anonymous" if they don't
+     */
     String getName(String usernameEmail) {
         usernameEmail = usernameEmail.trim();
         User user;
