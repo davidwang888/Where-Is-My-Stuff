18e7d3d219d3d0ca352580783a2152056e077d05
David <davidwang888@gmail.com>
2017-06-20 19:28:56 -0400

    javadoc-ed the model class

--- WhereIsMyStuff/app/src/main/java/cs2340/whereismystuff/model/ItemType.java
+++ WhereIsMyStuff/app/src/main/java/cs2340/whereismystuff/model/ItemType.java
@@ -1,6 +1,12 @@
 package cs2340.whereismystuff.model;
 
+/**
+ * A representation of all the types of items
+ */
 enum ItemType {
+    /**
+     * The different types an item can be
+     */
     TECHNOLOGICAL ("Technological"),
     FURNITURE ("Furniture"),
     RECREATIONAL ("Recreational"),
@@ -8,14 +14,19 @@ enum ItemType {
     PET ("Pet"),
     OTHER ("Other");
 
+    /**
+     * The string representation of the type
+     */
     private final String _type;
 
+
+    /**
+     * Creates a new item type
+     *
+     * @param ptype the name of the item type
+     */
     ItemType(String ptype) {
         _type = ptype;
-    }
-
-    String getType() {
-        return _type;
     }
 
     @Override
